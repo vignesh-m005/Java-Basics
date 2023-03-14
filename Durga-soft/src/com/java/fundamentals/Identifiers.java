2b9a23e9aa37f6e1c1b1379060e100fc8d7f8f72
vignesh.m <vmurugan@americanfirstfinance.com>
2023-03-14 14:45:59 +0530

    First

---
+++ Durga-soft/src/com/java/fundamentals/Identifiers.java
@@ -0,0 +1,13 @@
+package com.java.fundamentals;
+
+public class Identifiers {
+
+	public static void main(String[] args) {
+		int x =10;
+
+	}
+
+	// A name in java program is known as identifiers
+	// It can be method name, class name, variable name or lable name
+	// It is used for identification purpose
+}
