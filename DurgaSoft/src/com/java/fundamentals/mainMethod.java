1206e35747b9b21111f61308c9d2d22054ee2785
vignesh.m <vmurugan@americanfirstfinance.com>
2023-03-23 13:06:24 +0530

    mainMethod

---
+++ DurgaSoft/src/com/java/fundamentals/mainMethod.java
@@ -0,0 +1,18 @@
+package com.java.fundamentals;
+
+public class mainMethod {
+	
+	static{
+	      System.out.println("vjbfbv");
+//	      System.exit(0);
+	  }
+
+	public static final strictfp synchronized void main(String[] args) {
+		System.out.println("Valid main");
+		main(new int[] {1,2,3});
+
+	}
+	public static void main(int[] args) {
+		System.out.println("int[]");
+	}
+}
